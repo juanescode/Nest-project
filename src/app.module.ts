c2747d3bd8705946e947478bacd9a3831f78f173
juanescode <juanestebanvilladagallego23@gmail.com>
2024-07-17 20:37:38 -0500

    first project with Nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    username: 'root',
+    host: 'localhost',
+    password: '',
+    port: 3306,
+    database: 'nestdb',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }), UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { createUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+import { updateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.usersService.getUsers()
+  }
+
+  @Get(':id') 
+  getUser(@Param('id', ParseIntPipe) id: number) {
+    console.log(id)
+    console.log(typeof id)
+    return this.usersService.getUser(id)
+  }
+
+  @Post()
+  createUser(@Body() newUser: createUserDto) {
+    return this.usersService.createUser(newUser);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.deleteUser(id)
+  }
+
+  @Patch(':id') 
+  updateUser(@Param('id', ParseIntPipe) id:number, @Body() user: updateUserDto) {
+    return this.usersService.updateUser(id, user)
+  }
+}
